8263eac58d513f016bb0b2ec4f831937ec87e4c3
Farseensattique <136583984+Farseensattique@users.noreply.github.com>
2023-06-20 17:10:33 +0600

    Day3(Updated).cpp
    Updated version of Day3.cpp uploaded on June 16, 2023

--- Day3.cpp
+++ Day3.cpp
@@ -1,57 +1,61 @@
 #include<iostream>
 using namespace std;
+
 class Beverages
 {
 public:
-    int totalVariety;
+    int totalVariety=6;
     int *price;
     int *quantity;
-    int totalBill=0, vat=0.5, discount=0.10;
+    int totalBill=0;
+    float vat=0.05; 
+    float discount=0.1;
+
     void bill()
     {
         if(totalBill>500)
         {
-            totalBill=totalBill+(totalBill*vat);
+            totalBill+=totalBill*vat;
             if(totalBill>1000)
             {
-                totalBill=totalBill-(totalBill*discount);
+                totalBill-=totalBill*discount;
             }
         }
-        cout<<"Total Bill for the customer : "<<totalBill;
+        cout<<"Total Bill for the customer : "<<totalBill<< " BDT " << endl;
     }
-
 };
+
 int main()
 {
     Beverages b;
+    
     b.price=new int[b.totalVariety];
-    b.quantity=new int[100];
+    b.quantity=new int[b.totalVariety];
     string bname[b.totalVariety]={"Pepsi", "Fanta", "Coca-Cola", "Mountain Dew", "Mirinda", "Sprite"};
-    cout<<"Enter the number of beverages in stock";
-    cin>>b.totalVariety;
-    cout<<"Enter the prices of the beverages";
+
+    cout<<"Enter the prices of the individual beverages : << endl";
     for(int i=0;i<b.totalVariety;i++)
     {
+        cout<<"bname[i]<<" : ";
         cin>>b.price[i];
     }
 
-    cout<<"Prices of individual beverages are listed below : "<<endl;
-    for(int i=0;i<b.totalVariety;i++)
-    {
-        cout<<bname[i]<<" : "<<b.price[i]<<endl;
-    }
-
+    cout<<"Enter the quantities of sold beverages of each brand : "<<endl;
     for(int i=0;i<b.totalVariety;i++)
     {
         cout<<bname[i]<<" : ";
         cin>>b.quantity[i];
     }
+
     for(int i=0;i<b.totalVariety;i++)
     {
-        b.totalBill=b.price[i]*b.quantity[i];
+        b.totalBill+=b.price[i]*b.quantity[i];
     }
+    
     b.bill();
+    
     delete[] b.price;
     delete[] b.quantity;
+    
     return 0;
 }
